48ea34049962877950847c32556bc75bf15abcc6
Rajveer(Chiranjeev)Shekhawat <121601167+rajvveer@users.noreply.github.com>
2023-01-17 10:59:19 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = ({type}) => {
 
   useEffect(() => {
     const fetchVideos = async () => {
-      const res = await axios.get(`/videos/${type}`);
+      const res = await axios.get(`/api/videos/${type}`);
       setVideos(res.data);
     };
     fetchVideos();
